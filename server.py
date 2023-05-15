92c85a72a1a76d4b74fc9712a08eaa872802ea6a
Alexander Chaika <manti.by@gmail.com>
2023-05-15 21:38:34 +0300

    Add websocket

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
+import logging
 from json import JSONDecodeError
 
-from aiohttp import web
+from aiohttp import WSMessage, WSMsgType, web
 from aiohttp_apispec import docs, request_schema, setup_aiohttp_apispec
 from marshmallow import ValidationError
 
@@ -8,6 +9,9 @@ from message import send_message_to_all, send_message
 from schemas import MessageSchema
 
 routes = web.RouteTableDef()
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 @docs(
@@ -43,6 +47,27 @@ async def index_post(request: web.Request) -> web.Response:
 @routes.get("/")
 async def index_get(request: web.Request) -> web.Response:
     return web.json_response({"result": "OK"})
+
+
+@docs(
+   tags=["websocket"],
+   summary="Websocket endpoint",
+   description="Connect to websocket",
+)
+@routes.get("/ws")
+async def websockets(request):
+    ws = web.WebSocketResponse()
+    await ws.prepare(request)
+
+    async for msg in ws:  # type: WSMessage
+        if msg.type == WSMsgType.TEXT:
+            if msg.data == "/close":
+                await ws.close()
+            else:
+                await ws.send_str(msg.data)
+        elif msg.type == WSMsgType.ERROR:
+            logger.error(f"WS connection closed with exception {request.app.ws.exception()}")
+    return ws
 
 
 if __name__ == "__main__":
